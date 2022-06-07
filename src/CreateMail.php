c002f8d081ce50362ca7e2fe428d52fb0bb79209
SixSad <84086922+SixSad@users.noreply.github.com>
2022-06-07 09:47:42 +0700

    Update CreateMail.php

--- src/CreateMail.php
+++ src/CreateMail.php
@@ -7,7 +7,15 @@ use PHPMailer\PHPMailer\SMTP;
 
 class CreateMail
 {
-    public static function generate(string $email, string $code, string $status = "email"): PHPMailer
+    public static function generate(string $email, string $code, string $status = "email", string $body ="<h1>Здравствуйте!</h1>
+        <p>Вы получили это письмо, потому что на нашем сервисе для регистрации был указан адрес вашей электронной почты.</p>
+        <p>Для подтверждения вашего аккаунта введите код:<p>
+        <div style='background:#faf9fa;border:1px solid #dad8de;text-align:center;padding:5px;margin:0 0 5px 0;font-size:24px;line-height:1.5;width:80%'>$code</div>
+        С уважением, команда сервиса M.Connection.", string $altBody = "Здравствуйте!
+        Вы получили это письмо, потому что на нашем сервисе для регистрации был указан адрес вашей электронной почты.
+        Для подтверждения вашего аккаунта введите код:
+        $code
+        С уважением, команда сервиса M.Connection."): PHPMailer
     {
         $config = config("email");
 
@@ -31,17 +39,9 @@ class CreateMail
             ? "Подтверждение почты"
             : "Восстановление пароля";
 
-        $mail->Body    = "<h1>Здравствуйте!</h1>
-        <p>Вы получили это письмо, потому что на нашем сервисе для регистрации был указан адрес вашей электронной почты.</p>
-        <p>Для подтверждения вашего аккаунта введите код:<p>
-        <div style='background:#faf9fa;border:1px solid #dad8de;text-align:center;padding:5px;margin:0 0 5px 0;font-size:24px;line-height:1.5;width:80%'>$code</div>
-        С уважением, команда сервиса M.Connection.";
+        $mail->Body = $body;
 
-        $mail->AltBody = "Здравствуйте!
-        Вы получили это письмо, потому что на нашем сервисе для регистрации был указан адрес вашей электронной почты.
-        Для подтверждения вашего аккаунта введите код:
-        $code
-        С уважением, команда сервиса M.Connection.";
+        $mail->AltBody = $altBody;
 
         $mail->CharSet = "UTF-8";
 
