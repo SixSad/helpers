40a8d259104ce317a06646c464156d93ae764a08
SixSad <84086922+SixSad@users.noreply.github.com>
2022-08-03 14:02:26 +0700

    Update AbstractListener.php

--- src/AbstractListener.php
+++ src/AbstractListener.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Log;
 
 abstract class AbstractListener
 {
-    public function handle($event): void
+    public function handle(AbstractEvent $event): void
     {
         Log::info(sprintf("Listener [%s] event [%s]", get_class($this), get_class($event)));
     }
